f69e56685725bc531cc44cf53f332bceedcef23e
Dona <dona.denkey_dandjein@edu.devinci.fr>
2022-03-23 13:53:21 +0100

    view to create course

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NewCourseView from '../views/NewCourseView.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/new-courses',
+    name: 'newCourses',
+    component: NewCourseView
   },
   {
     path: '/about',
